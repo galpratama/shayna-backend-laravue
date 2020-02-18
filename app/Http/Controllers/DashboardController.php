00e8d7be835468252d6639f894f77f3f68b3ee86
Galih Pratama <hanamura.iost@gmail.com>
2020-02-19 01:35:28 +0700

    Dashboard Statistics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -18,6 +19,20 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('pages.dashboard');
+        $income = Transaction::where('transaction_status','SUCCESS')->sum('transaction_total');
+        $sales = Transaction::count();
+        $items = Transaction::with('details')->orderBy('id','DESC')->take(5)->get();
+        $pie = [
+            'pending' => Transaction::where('transaction_status','PENDING')->count(),
+            'failed' => Transaction::where('transaction_status','FAILED')->count(),
+            'success' => Transaction::where('transaction_status','SUCCESS')->count(),
+        ];
+
+        return view('pages.dashboard')->with([
+            'income' => $income,
+            'sales' => $sales,
+            'items' => $items,
+            'pie' => $pie
+        ]);
     }
 }
