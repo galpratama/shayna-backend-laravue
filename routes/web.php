a1cf0f0a3f358a2f7fc555b0950878c4a9265129
Galih Pratama <hanamura.iost@gmail.com>
2020-03-06 08:24:25 +0700

    Enable register

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'DashboardController@index')->name('dashboard');
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
 Route::get('products/{id}/gallery', 'ProductController@gallery')
     ->name('products.gallery');
